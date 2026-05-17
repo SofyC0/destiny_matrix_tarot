affbf0602288c28766ae5ed6c503c8dbf9c14149
SofyC0 <sofya_shatova@mail.ru>
2026-05-17 18:30:24 +0300

    mz

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
-# backend/run.py
-import sys
-import os
-
-# Это самая важная строка
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".")))
-
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+
+    uvicorn.run(
+        "app.main:app",
+        host="127.0.0.1",
+        port=8000,
+        reload=True
+    )
